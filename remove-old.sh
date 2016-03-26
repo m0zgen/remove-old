ca96f88920750dfaf0b74bfe7ac5cfc189f86419
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-03-26 14:56:12 +0600

    Clear syntax, fix

--- remove-old.sh
+++ remove-old.sh
@@ -1,61 +1,35 @@
 #!/bin/sh
 
-# Use argument for remove old files. Example: ./remove-old.sh path howold
-# ./remove-old.sh /var/log 90
-# ./remove-old.sh /mnt/dta3/tmp/ 10 --delemptydir
 # Author: Yevgeniy Goncharov aka xck, http://sys-admin.kz
+# ./remove-old.sh /var/log 90
 # v2
 
 PATH=$1
 
+# if day empty
 if [[ -z $2 ]]; then
     OLD=""
 else
     OLD="-mtime +$2"
 fi
 
-#TYPE=$3
-
-# Send errors to null ) Test
-#/usr/bin/find $PATH -type f -mtime +OLD -exec rm {} \; 2>/dev/null
-
-#/usr/bin/find $PATH -type f -mtime +$OLD -exec /usr/bin/rm '{}' \;
-#/usr/bin/find $PATH -type d -mtime +$OLD -exec /usr/bin/rm -rf '{}' \;
-
-# for i in `/usr/bin/find $PATH -maxdepth 1 -type f -mtime +$OLD -print`; do
-
-# find /mnt/dta4/tmp/* -mtime +60 -print0 | xargs -0 rm -f
-
-#for i in `/usr/bin/find $PATH -type f -mtime +$OLD -print`; do
-#    echo -e "Deleting directory $i";
-#    /usr/bin/rm -rf $i;
-#done
-
+# delete empty directories
 del_empty_dir(){
     /usr/bin/find $PATH -empty -type d -delete >/dev/null 2>&1
     # echo Empty folders removed!
 }
 
+# delete empty files
 del_empty_files(){
     /usr/bin/find $PATH -empty -type f -delete >/dev/null 2>&1
     # echo Empty files removed!
 }
 
-# for i in "$@" ; do
+# try delete all
+del(){
+    /usr/bin/find $PATH $OLD -exec /usr/bin/rm -rf '{}' \; >/dev/null 2>&1
+}
 
-#     if [[ $i == "--delemptydir" ]] ; then
-#         del_empty_dir
-#         break
-#     fi
-
-# if [[ $i == "--delemptyfile" ]] ; then
-#         del_empty_files
-#         break
-#     fi
-
-# done
-
-/usr/bin/find $PATH $OLD -exec /usr/bin/rm -rf '{}' \; >/dev/null 2>&1
-
+del
 del_empty_files
 del_empty_dir
