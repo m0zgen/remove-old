8518e20d708feb3fe7f8dfee10408486d4412ea7
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-05-24 22:10:45 +0600

    Add log, add delete hidden

--- remove-old.sh
+++ remove-old.sh
@@ -7,53 +7,58 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# if day empty
-if [[ -z $1 ]]; then
-  echo "Error - Set destination. Example - ./remove-old.sh /var/log 90"
-  exit 1
-else
-  DEST=$1
-fi
+# ------------------------------------------ LOGS
+LOGFILE="/var/log/remove-old.log"
 
-# if day empty
-if [[ -z $2 ]]; then
-  echo "Error - Set oldest time in days. Example - ./remove-old.sh /var/log 90"
-  exit 1
-else
-  OLD="$2"
+# check log file
+if [[ ! -e $LOGFILE ]]; then
+  touch $LOGFILE
+elif [[ ! -d $LOGFILE ]]; then
+  echo "$(date +%Y%m%d+%H:%M:%S) : Start remove" >> $LOGFILE
 fi
+# ------------------------------------------ LOGS
 
+# arguments is empty
 if [ -z $1 ] || [ -z $2 ] ; then
   echo "Please use script with example - ./remove-old.sh /var/log 90"
 else
+
+  DEST=$1
+  OLD=$2
   DELETE_DAY_FILES=`/usr/bin/find $DEST -type f -mtime +$OLD`
 
   if [[ ! -z $DELETE_DAY_FILES ]]; then
-	# delete empty directories
-	del_empty_dir(){
-	  find $DEST -empty -type d -delete >/dev/null 2>&1
-	# echo Empty folders removed!
-  }
+    echo "$DELETE_DAY_FILES" >> $LOGFILE
 
-# delete empty files
-del_empty_files(){
-  find $DEST -empty -type f -delete >/dev/null 2>&1
-	# echo Empty files removed!
-  }
+    # delete empty and hidden directories
+    del_empty_dir(){
+      find $DEST -empty -type d -delete >/dev/null 2>&1
+      find $DEST -type d -name ".*" -delete >/dev/null 2>&1
+    # echo Empty folders removed!
+    }
 
-# try delete all
-del(){
-	#find $DEST $OLD -exec rm -rf '{}' \; >/dev/null 2>&1
-	find $DEST/* -mtime +$OLD -print0 | xargs -0 rm -rf 2>&1 >/dev/null
-  }
+    # delete empty and hidden files
+    del_empty_files(){
+      find $DEST -empty -type f -delete >/dev/null 2>&1
+      find $DEST -type f -name ".*" -delete >/dev/null 2>&1
+    # echo Empty files removed!
+    }
 
-  del
-  del_empty_files
-  del_empty_dir
+    # try delete all
+    del(){
+    # find $DEST $OLD -exec rm -rf '{}' \; >/dev/null 2>&1
+    # find $DEST/* -mtime +$OLD -print0 | xargs -0 rm -rf 2>&1 >/dev/null
+    find $DEST -mtime +$OLD -print0 | xargs -0 rm -rf 2>&1 >/dev/null
+    }
+
+    del
+    del_empty_files
+    del_empty_dir
+
+  fi
+
 fi
 
-fi
-
+echo "$(date +%Y%m%d+%H:%M:%S) : Finished" >> $LOGFILE
 exit
-
 
